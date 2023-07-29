d51f9ae226b16100324c7becf2033f9b4f59069d
DeepakTiwari76 <deepakpandat0655@gmail.com>
2023-07-29 17:10:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Home from './componants/Home';
+import Header from './componants/Header';
+import Footer from './componants/Footer'; 
+
+function App() {
+  return (
+    <div>
+
+  <Header/>
+  <Home/>
+  <Footer/>
+
+    </div>
+  );
+}
+
+export default App;
